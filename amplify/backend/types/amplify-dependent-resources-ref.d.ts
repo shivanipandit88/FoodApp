f6d036cedb0ca993de1267ee36a9b218f55e2dd4
satish kathiriya sjsu <satish.kathiriya@sjsu.edu>
2021-11-19 16:39:38 -0800

    added order api and table

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "fassos33d8fe4b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger01242f38": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "food": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
