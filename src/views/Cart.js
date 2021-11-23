05f7b30d31e171ac8ec0b22a3117fb31717661a4
Chirag Rajpal <chiragbrajpal@gmail.com>
2021-11-23 15:03:41 -0800

    Cart Redirect

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -1,70 +1,82 @@
-import { API, Auth,graphqlOperation } from "aws-amplify";
+import { API, Auth, graphqlOperation } from "aws-amplify";
 import React, { useState, useEffect } from "react";
 import { useHistory, Link, useLocation } from "react-router-dom";
 import { Container, Row, Col, Table, Image, Button } from "react-bootstrap";
 import { listCartDatas } from "../graphql/queries";
-import {deleteCartData } from "../graphql/mutations"
+import { deleteCartData } from "../graphql/mutations";
 // import { API, Auth, graphqlOperation } from 'aws-amplify';
 export default function Cart() {
   const location = useLocation();
-  const [userData, setUserData] = useState('');
+  const [userData, setUserData] = useState("");
   const [cartItem, setCartItem] = useState([]);
 
-  useEffect( () => {
-    const fetchCartData = async() =>{
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchCartData = async () => {
       // console.log("user",location.state.id.payload.username)
-          await API.graphql(graphqlOperation(listCartDatas,{
-            filter: {
-                username: {
-                    eq : location.state.id.payload.username
-                }
-            }
-          }))
-          .then((apiData) => {
-            setCartItem(apiData.data.listCartDatas.items)
-            console.log(cartItem)
-          })
-          .catch((e) => console.log("No apidata", e));
-    }
+      await API.graphql(
+        graphqlOperation(listCartDatas, {
+          filter: {
+            username: {
+              eq: location.state.id.payload.username,
+            },
+          },
+        })
+      )
+        .then((apiData) => {
+          setCartItem(apiData.data.listCartDatas.items);
+          console.log(cartItem);
+        })
+        .catch((e) => console.log("No apidata", e));
+    };
 
-    
-      fetchCartData();
-      }, [setCartItem] );
-  
-    
+    fetchCartData();
+  }, [setCartItem]);
 
-    async function placeOrder() {
-      const map = new Map();
-      console.log("cart1",cartItem)
+  async function placeOrder() {
+    const map = new Map();
+    console.log("cart1", cartItem);
 
-      cartItem.map(item => {
-        
-        var todoDetails = {
-            id: item.id
-          };
-          console.log("todo",todoDetails)
-         API.graphql({ query: deleteCartData, variables: {input: todoDetails}})
-        
-      })
-      alert('order has been placed');
-      setCartItem([]);
-    }
+    cartItem.map((item) => {
+      var todoDetails = {
+        id: item.id,
+      };
+      console.log("todo", todoDetails);
+      API.graphql({ query: deleteCartData, variables: { input: todoDetails } });
+    });
+    alert("order has been placed");
+    setCartItem([]);
+  }
+
+  function routeChange(){ 
+    let path = `/#`; 
+    console.log("route CHange called")
+    history.push(path);
+  }
+
 
   return (
     <div className="main menu">
       <Container>
         <h1>Cart {userData}</h1>
-        <button onClick={ ()=>placeOrder() } className="btn btn-dark">Place Order</button>
+        <button
+          onClick={() => {
+            placeOrder();
+            routeChange();
+          }}
+          className="btn btn-dark"
+        >
+          Place Order
+        </button>
         <Table striped bordered hover responsive="sm">
           <tbody>
             {cartItem &&
               cartItem.map((item) => (
                 <tr>
-                <td>{item.menu.dishname}</td>
-                  <td>
-                    {item.menu.ingredients}
-                  </td>
-                  <td>$ {item.menu.price}</td> 
+                  <td>{item.menu.dishname}</td>
+                  <td>{item.menu.ingredients}</td>
+                  <td>$ {item.menu.price}</td>
                 </tr>
               ))}
 
--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,123 +1,159 @@
-import { Auth, Storage, API, graphqlOperation } from 'aws-amplify';
-import React, { useState, useEffect } from 'react';
+import { Auth, Storage, API, graphqlOperation } from "aws-amplify";
+import React, { useState, useEffect } from "react";
 import { useHistory, Link, useLocation } from "react-router-dom";
 import { Container, Row, Col, Table, Image, Button } from "react-bootstrap";
-import food5 from '../assets/food5.jpg';
-import { getRestaurant, listAddMenus, listRestaurants } from '../graphql/queries';
-import { createCartData } from '../graphql/mutations';
-
+import food5 from "../assets/food5.jpg";
+import {
+  getRestaurant,
+  listAddMenus,
+  listRestaurants,
+} from "../graphql/queries";
+import { createCartData } from "../graphql/mutations";
 
 export default function Menu() {
-    const location = useLocation();
-    const [menus, setmenus] = useState([]);
-    const [restaurants, setRestaurants] = useState({
-        username: ""
-    });
-    const [userData, setUserData] = useState({ payload: { username: '' } });
+  const location = useLocation();
+  const [menus, setmenus] = useState([]);
+  const [restaurants, setRestaurants] = useState({
+    username: "",
+  });
+  const [userData, setUserData] = useState({ payload: { username: "" } });
+  const [addMenuBtn, setaddMenuBtn] = useState(false);
+  const [cartCount, setCartCount] = useState(0);
 
-    const [addMenuBtn, setaddMenuBtn] = useState(false);
+  useEffect(() => {
+    fetchRestaurants();
+    fetchUserData();
+    fetchMenus();
+  }, []);
 
-    useEffect(() => {
-        fetchRestaurants();
-        fetchUserData();
-        fetchMenus();
-      }, []);
-    
-      async function fetchMenus() {
-        const apiData = await API.graphql(graphqlOperation(listAddMenus, {
-            filter: {
-                resid: {
-                    eq: location.state.state.id
-                }
-            }
-        }));
-        console.log("Menu Data ")
-        console.log(apiData)
-        setmenus(apiData.data.listAddMenus.items);
-      }
-    
-      async function fetchRestaurants() {
-        const apiData = await API.graphql(graphqlOperation(getRestaurant, {id: location.state.state.id}));
-        console.log("Restaurant Data ");
-        console.log(apiData);
-        setRestaurants(apiData.data.getRestaurant);
-    }
-    const history = useHistory();
-    
-      async function fetchUserData() {
-        await Auth.currentAuthenticatedUser()
-          .then((userSession) => {
-            console.log("userData: ", userSession);
-            setUserData(userSession.signInUserSession.accessToken);
-            
-          })
-          .catch((e) => console.log("Not signed in", e));
-      }
+  useEffect(() => {}, [cartCount]);
 
-      async function addtoCart(menuID){
-        try {
-            console.log(menuID)
-            await API.graphql(graphqlOperation(createCartData, {input: { menuID: menuID, username: userData.payload.username}}));      
-            alert("Item Added to Cart");
-            
-          } catch(e)
-          {
-            console.error('error adding to Cart', e);
-            // setErrorMessages(e.errors);
-          }
-      }
-    
-    function RenderAddMenuButton()
-    {
-        if(restaurants.username === userData.payload.username)
-        {
-            return(
-                <Button className="btn btn-dark" onClick={() => {
-                    history.push('/addmenu', { state: { id: restaurants.id } })
-                }}>Add Menu</Button>
-            )
-        }
-        return(
-            <></>
-        )
-    }
-    
-    return (
-        <div>
-            <div className="banner">
-                <Image src={food5} alt="Restaurant" />
-                <p>{restaurants.name}</p>
-            </div>
-            <div className='main menu'>
-                <Container>
-                    <div className="header">
-                    <h1>Menu</h1>
-                    <RenderAddMenuButton />
-                    </div>
-                    <Table striped bordered hover responsive="sm">
-                        <tbody>
-                            {
-                                menus.map(menus => (
-                                <tr>
-                                    <td><Image src={'https://d2pmgib90mmdnn.cloudfront.net/public/' + menus.image} alt="Restaurant 1" /></td>
-                                    <td>{menus.dishname} <span>{menus.ingredients}</span></td>
-                                    <td>$ {menus.price}</td>
-                                    <td><button onClick={()=>addtoCart(menus.id)} className="btn btn-dark">Add to Cart</button></td>
-                                    {/* <td><Link to="/cart" className="btn btn-dark" >Add to Cart</Link></td> */}
-                                </tr>
-                                ))
-                            }
-                        </tbody>
-                    </Table>
-                    <Row>
-                        <div className="btns">
-                            <Link to="/" className="btn btn-dark">Home</Link>
-                            <Link to= {{pathname: "/cart", state: { id: userData } }} className="btn btn-warning">Go to Cart</Link>
-                        </div>
-                    </Row>
-                </Container>
-            </div>
-
-        </div>
+  async function fetchMenus() {
+    const apiData = await API.graphql(
+      graphqlOperation(listAddMenus, {
+        filter: {
+          resid: {
+            eq: location.state.state.id,
+          },
+        },
+      })
     );
+    console.log("Menu Data ");
+    console.log(apiData);
+    setmenus(apiData.data.listAddMenus.items);
+  }
+
+  async function fetchRestaurants() {
+    const apiData = await API.graphql(
+      graphqlOperation(getRestaurant, { id: location.state.state.id })
+    );
+    console.log("Restaurant Data ");
+    console.log(apiData);
+    setRestaurants(apiData.data.getRestaurant);
+  }
+  const history = useHistory();
+
+  async function fetchUserData() {
+    await Auth.currentAuthenticatedUser()
+      .then((userSession) => {
+        console.log("userData: ", userSession);
+        setUserData(userSession.signInUserSession.accessToken);
+      })
+      .catch((e) => console.log("Not signed in", e));
+  }
+
+  async function addtoCart(menuID) {
+    try {
+      console.log(menuID);
+      await API.graphql(
+        graphqlOperation(createCartData, {
+          input: { menuID: menuID, username: userData.payload.username },
+        })
+      );
+      alert("Item Added to Cart");
+    } catch (e) {
+      console.error("error adding to Cart", e);
+      // setErrorMessages(e.errors);
+    }
+  }
+
+  function RenderAddMenuButton() {
+    if (restaurants.username === userData.payload.username) {
+      return (
+        <Button
+          className="btn btn-dark"
+          onClick={() => {
+            history.push("/addmenu", { state: { id: restaurants.id } });
+          }}
+        >
+          Add Menu
+        </Button>
+      );
+    }
+    return <></>;
+  }
+
+  return (
+    <div>
+      <div className="banner">
+        <Image src={food5} alt="Restaurant" />
+        <p>{restaurants.name}</p>
+      </div>
+      <div className="main menu">
+        <Container>
+          <div className="header">
+            <h1>Menu</h1>
+            <RenderAddMenuButton />
+          </div>
+          <Table striped bordered hover responsive="sm">
+            <tbody>
+              {menus.map((menus) => (
+                <tr>
+                  <td>
+                    <Image
+                      src={
+                        "https://d2pmgib90mmdnn.cloudfront.net/public/" +
+                        menus.image
+                      }
+                      alt="Restaurant 1"
+                    />
+                  </td>
+                  <td>
+                    {menus.dishname} <span>{menus.ingredients}</span>
+                  </td>
+                  <td>$ {menus.price}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        addtoCart(menus.id);
+                        setCartCount(cartCount + 1);
+                      }}
+                      className="btn btn-dark"
+                    >
+                      Add to Cart
+                    </button>
+                  </td>
+                  {/* <td><Link to="/cart" className="btn btn-dark" >Add to Cart</Link></td> */}
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Row>
+            <div className="btns">
+              <Link to="/" className="btn btn-dark">
+                Home
+              </Link>
+              <div>Items to checkout: {cartCount}</div>
+              <Link
+                to={{ pathname: "/cart", state: { id: userData } }}
+                className="btn btn-warning"
+              >
+                Go to Cart {cartCount}
+              </Link>
+            </div>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
